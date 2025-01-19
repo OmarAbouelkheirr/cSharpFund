44bc8f97762e5fe9b3a484032bc4a7818f25ee23
Omar Aboelkheir <OmarrAbouelkheir@gmail.com>
2025-01-19 17:43:07 +0200

    Add files via upload

---
+++ TrainingPalindromeWords/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace CSharpFund.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of array elements: ");
+            int numberOfElements = int.Parse(Console.ReadLine());
+            int[] Array = new int[numberOfElements];
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Console.WriteLine($"Enter element {i+1}: ");
+                Array[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            Console.WriteLine("-.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-");
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Console.WriteLine($"Element number {i + 1} is {Array[i]}");
+            }
+
+            Console.WriteLine("-.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-");
+
+            int sum = 0 , smallestNumber = int.MaxValue , greatestNumber = 0 ;
+
+            foreach (int x in Array)
+            {
+                sum += x;
+                if (x < smallestNumber)
+                    smallestNumber = x;
+             
+                if (x >  greatestNumber)
+                    greatestNumber = x;
+            }
+
+            double avg = sum / numberOfElements;
+            Console.WriteLine($"Avg: {avg}");
+            Console.WriteLine($"Smallest Number: {smallestNumber}");
+            Console.WriteLine($"Greatest Number: {greatestNumber}");
+        }
+    }
+}
