f26c7b67d9a6e0ead53e0b4523dbf9087d006a55
Omar Aboelkheir <OmarrAbouelkheir@gmail.com>
2025-01-19 17:55:57 +0200

    Add files via upload

--- TrainingPalindromeWords/Program.cs
+++ TrainingPalindromeWords/Program.cs
@@ -4,42 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter number of array elements: ");
-            int numberOfElements = int.Parse(Console.ReadLine());
-            int[] Array = new int[numberOfElements];
+            Console.WriteLine("Enter Word:");
+            string word = Console.ReadLine();
 
-            for (int i = 0; i < numberOfElements; i++)
+            bool palindrome = true;
+            for (int i = 0; i < word.Length / 2; i++)
             {
-                Console.WriteLine($"Enter element {i+1}: ");
-                Array[i] = int.Parse(Console.ReadLine());
+                if (word[i] != word[word.Length - i - 1]) 
+                {
+                    palindrome = false;
+                    break; 
+                }
             }
 
+            string result = palindrome ? "Palindrome" : "Not palindrome";
 
-            Console.WriteLine("-.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-");
-
-            for (int i = 0; i < numberOfElements; i++)
-            {
-                Console.WriteLine($"Element number {i + 1} is {Array[i]}");
-            }
-
-            Console.WriteLine("-.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.--.-");
-
-            int sum = 0 , smallestNumber = int.MaxValue , greatestNumber = 0 ;
-
-            foreach (int x in Array)
-            {
-                sum += x;
-                if (x < smallestNumber)
-                    smallestNumber = x;
-             
-                if (x >  greatestNumber)
-                    greatestNumber = x;
-            }
-
-            double avg = sum / numberOfElements;
-            Console.WriteLine($"Avg: {avg}");
-            Console.WriteLine($"Smallest Number: {smallestNumber}");
-            Console.WriteLine($"Greatest Number: {greatestNumber}");
+            Console.WriteLine($"{word} is {result}");
         }
     }
 }
